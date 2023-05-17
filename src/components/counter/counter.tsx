dfc96046da728d4c9ae7de753fa6bea1b3ca3f96
dv-joan <joan300501@gmail.com>
2023-05-17 12:49:52 -0500

    feat: State Management Practice

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -13,30 +13,56 @@ export default component$(() => {
             decrement: $(function () { this.contador-- })
         }
     );
-    return <div>
-        <section class='card'>
+    return <div class='grid grid-flow-col gap-3'>
 
-            <h1 class="titles">w/useSignal</h1>
+        <section class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">w/useSignal</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Counter increment/decrement classic example with useSignal qwik module.</p>
+            <button onClick$={() => count.value++} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Increment
 
-            <button onClick$={() => count.value--}>-</button>
-            {count}
-            <button onClick$={() => count.value++}>+</button>
+            </button>
+            <span class="mx-2 text-gray-600 dark:text-gray-300">{count}</span>
+            <button onClick$={() => count.value--} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Decrement
+
+            </button>
         </section>
-        <section class='card'>
+        <section class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">w/useStore</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Counter increment/decrement classic example with useSignal qwik module.</p>
+            <button onClick$={() => store.counter++} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Increment
 
-            <h1 class='titles'>w/useStore</h1>
-            <button onClick$={() => store.counter--}>-</button>
-            {store.counter}
-            <button onClick$={() => store.counter++}>+</button>
+            </button>
+            <span class="mx-2 text-gray-600 dark:text-gray-300">{store.counter}</span>
+            <button onClick$={() => store.counter--} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Decrement
+
+            </button>
+        </section>
+        <section class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">useStore w/Functions</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Counter increment/decrement classic example with useSignal qwik module.</p>
+            <button onClick$={() => functionStore.increment()} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Increment
+
+            </button>
+            <span class="mx-2 text-gray-600 dark:text-gray-300">{functionStore.contador}</span>
+            <button onClick$={() => functionStore.decrement()} class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Decrement
+
+            </button>
         </section>
 
-        <section class='card'>
 
-            <h1 class='titles'>useStore w/Functions</h1>
-            <button onClick$={() => functionStore.decrement()}>-</button>
-            {functionStore.contador}
-            <button onClick$={() => functionStore.increment()}>+</button>
-        </section>
+
 
     </div>;
 });
